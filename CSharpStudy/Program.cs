4e407f74f14297db1b1448d9ba01c2fcfba822b9
junki <kjgcde@naver.com>
2020-06-14 13:48:40 +0900

    초기 커밋.

---
+++ CSharpStudy/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //// 기본 변수
+            //short shortNum = 0;
+
+            //int intNum = 0;
+
+            //double doubleNum = 0;
+
+            //float floatNum = 0;
+
+            //string stringText = "";
+
+            //char charText = 'a';
+
+            //bool boolVal = true;
+
+            //// dotnet 변수
+            //Int16 dotNetShort = 0;
+
+            //Int32 dotNetInt = 0;
+
+            //Int64 dotNetDouble = 0;
+
+            //String dotNetString = "";
+
+            // 동적 타입
+            var dynamicType = false;
+        }
+    }
+}
